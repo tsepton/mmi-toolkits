0e5a36427dd3eb27d2481d4d014d349b8bbff775
Thibaut Septon <tsepton@gmail.com>
2026-02-02 12:41:25 +0100

    Add deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',
+  basePath: '/mmi-toolkits',
+  assetPrefix: '/mmi-toolkits/',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
