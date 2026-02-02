361a54ebd9581d0a7620422447339cec80376bbb
tsepton <tsepton@users.noreply.github.com>
2026-02-02 12:10:07 +0000

    deploy: 0e5a36427dd3eb27d2481d4d014d349b8bbff775

---
+++ _next/static/3jh7eSZfqrbpc0gRsjUi4/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mmi-toolkits//_next/:path+",
+        "destination": "/mmi-toolkits/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
