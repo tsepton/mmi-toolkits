1f2b67247cce37419a99f2838d7dc91f9cd29450
Thibaut Septon <tsepton@gmail.com>
2026-02-02 14:34:19 +0100

    Add margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center">
-        <h1 className="text-3xl">Multimodal Interaction Toolkits</h1>
+        <div className="text-3xl my-8">Multimodal Interaction Toolkits</div>
         <Table />
       </main>
     </div>
